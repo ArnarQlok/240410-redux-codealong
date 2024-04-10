ce45c19c158e3116c629166d0439b5445ec96a42
Arnar Johannsson <arnar.johannsson@qlok.se>
2024-04-10 10:43:16 +0200

    added useDispatch and useSelector to the app

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,14 +1,28 @@
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+
 const Todos = () => {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
   return (
     <>
       <div className="self-center">Todos</div>
       <ul className="list-none">
-        <li className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded">
-          <div className="text-white">Jag är en todo</div>
-          <button className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">
-            Delete
-          </button>
-        </li>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+          >
+            <div className="text-white">{todo.text}</div>
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+            >
+              Delete
+            </button>
+          </li>
+        ))}
       </ul>
     </>
   );
